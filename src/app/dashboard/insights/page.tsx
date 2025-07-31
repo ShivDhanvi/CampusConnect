bcc29ab641d299ea374a12651314c172043b7500
shivaramprasad software-developer <shivaramtechdev@gmail.com>
2025-07-31 17:56:09 +0000

    Still empty nothing is showing over there

--- src/app/dashboard/insights/page.tsx
+++ src/app/dashboard/insights/page.tsx
@@ -140,7 +140,7 @@ export default function InsightsPage() {
             </div>
 
             <div className="grid gap-6 md:grid-cols-5">
-                <Card className="md:col-span-2">
+                <Card className="md:col-span-2 flex flex-col">
                     <CardHeader>
                         <CardTitle>User Demographics</CardTitle>
                         <CardDescription>Distribution of user roles in the system.</CardDescription>
