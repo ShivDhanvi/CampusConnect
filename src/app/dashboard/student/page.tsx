63bdffa32ad2364b5226038a2ceefda9a33c2c94
shivaramprasad software-developer <shivaramtechdev@gmail.com>
2025-08-02 19:52:36 +0000

    inside main element I see the sidebar div and right side content div whi

--- src/app/dashboard/student/page.tsx
+++ src/app/dashboard/student/page.tsx
@@ -1,8 +1,6 @@
 
 export default function StudentDashboardPage() {
     return (
-        <div className="space-y-8">
-            {/* The content for the student dashboard will be added here. */}
-        </div>
+        <></>
     )
 }
